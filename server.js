d299c30a53d5178b8a583db253b6c9f2107ce6b7
cmac88 <cmclaughlin88@gmail.com>
2018-06-25 10:24:08 -0700

    extra fix on gitignore

--- server.js
+++ server.js
@@ -2,11 +2,11 @@ const express = require('express');
 const path = require('path');
 const bp = require('body-parser')
 const app = express();
-const router = require('./router')
+const router = require('./server/router')
 app.use(bp.json());
 router.router(app);
 
-app.use(express.static(path.join(__dirname, "../public/dist/public")))
-app.all('*', (req, res)=>res.sendFile(path.resolve(__dirname, '../public/dist/public/index.html')))
+app.use(express.static(path.join(__dirname, "/public/dist/public")))
+app.all('*', (req, res)=>res.sendFile(path.join(__dirname, '/public/dist/public/index.html')))
 
 app.listen(8000, ()=>console.log('listening'))
